f80a867bee026481cda38b07e7d24de1e8024e86
Nima Kaviani <nkavian@us.ibm.com>
2019-07-31 14:52:10 -0700

    bump the linting deadline

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-golangci-lint run --exclude "cyclomatic complexity" --exclude "weak cryptographic primitive" --exclude "should not be capitalized" -v
+golangci-lint run --deadline 2m --exclude "cyclomatic complexity" --exclude "weak cryptographic primitive" --exclude "should not be capitalized" -v
