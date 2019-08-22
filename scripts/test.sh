cac562f204dd18972f5c4d7bf9cc6df0cca580ce
Oleksandr Slynko <oslynko@pivotal.io>
2019-08-22 11:36:02 +0100

    Allow passing custom arguments to test script
    such as -untilItFails
    
    Signed-off-by: Akshay Mankar <amankar@pivotal.io>

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 export GO111MODULE=on
-ginkgo -mod=vendor -race -p -randomizeAllSpecs -randomizeSuites -r -skipPackage integration .
+ginkgo -mod=vendor -race -p -randomizeAllSpecs -randomizeSuites -r -skipPackage integration "$@" .
