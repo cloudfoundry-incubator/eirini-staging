406bc5664339930f3b2bd0e0ac02e81b52156d46
Ulrich Kramer <u.kramer@sap.com>
2019-10-02 08:37:57 +0200

    Show more details on completion error

--- responder.go
+++ responder.go
@@ -148,7 +148,12 @@ func (r Responder) sendCompleteResponse(response *models.TaskCallbackResponse) e
 	defer resp.Body.Close()
 
 	if resp.StatusCode >= 400 {
-		return errors.New("Request not successful")
+		body, err := ioutil.ReadAll(resp.Body)
+		var message string
+		if err == nil {
+			message = string(body)
+		}
+		return fmt.Errorf("request not successful: uri=%s status=%d %s", uri, resp.StatusCode, message)
 	}
 
 	return nil
