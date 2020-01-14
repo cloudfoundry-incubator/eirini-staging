a7ee4d43dc69e54b01cfbb7bf9bbe672c37fc5d0
Oleksandr Slynko <oslynko@pivotal.io>
2020-01-14 10:40:41 +0000

    Remove old scripts & add one for integration tests
    [#168728857]

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -1,10 +1,2 @@
-#! /bin/bash
-
-set -exuo pipefail
-
-WORKSPACE=$(pwd)
-
-docker build -t eirinistaging/integration-tests:latest -f ./image/integration-tests/Dockerfile .
-docker run -it --rm  \
-  -v ${WORKSPACE}:/eirinistaging \
-  eirinistaging/integration-tests bash
+#!/usr/bin/env bash
+ginkgo -mod=vendor -race -p -randomizeAllSpecs -randomizeSuites -r "$@" integration
--- scripts/lint.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-golangci-lint run --deadline 2m --exclude "cyclomatic complexity" --exclude "weak cryptographic primitive" --exclude "should not be capitalized" -v
