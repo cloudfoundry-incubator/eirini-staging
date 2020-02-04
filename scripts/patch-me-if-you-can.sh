4adbc426ccb9e8b935872c1b6233f1edd38f96af
Georgi Dankov <gddankov@gmail.com>
2020-02-04 09:43:51 +0100

    Add patch script for staging images
    [#170873546]
    
    Signed-off-by: Julian Skupnjak <skupnjak@de.ibm.com>

---
+++ scripts/patch-me-if-you-can.sh
@@ -0,0 +1,116 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+IFS=$'\n\t'
+
+readonly USAGE="Usage: patch-me-if-you-can.sh --cluster <cluster-name> [ <component-name> ... ]"
+readonly SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+readonly EIRINI_STAGING_BASEDIR=$(realpath "$SCRIPT_DIR/..")
+readonly EIRINI_RELEASE_BASEDIR=$(realpath "$SCRIPT_DIR/../../eirini-release")
+
+
+main() {
+  check_flags "$@"
+  shift 2
+
+  export VALUES_FILE="$HOME/workspace/eirini-private-config/environments/kube-clusters/$(current_cluster_name)/scf-config-values.yaml"
+  if [ "$#" == "0" ]; then
+    echo "No components specified. Nothing to do."
+    exit 0
+  fi
+
+  local component
+  for component in "$@"; do
+    echo "--- Patching component $component ---"
+    docker_build "$component"
+    docker_push "$component"
+    update_values_file "$component"
+  done
+
+  helm_upgrade
+  restart_eirini
+}
+
+check_flags() {
+  if [ "$#" == "0" ]; then
+    echo "$USAGE"
+    exit 1
+  fi
+
+  if [ "$1" != "--cluster" ] && [ "$1" != "-c" ]; then
+    echo "$USAGE"
+    exit 1
+  fi
+
+  local cluster_name
+  cluster_name=$2
+
+  if [ -z "$cluster_name" ]; then
+    echo "$USAGE"
+    exit 1
+  fi
+
+  if [ "$cluster_name" != "$(current_cluster_name)" ]; then
+    echo "Your current cluster is $(current_cluster_name), but you want to update $cluster_name. Please target $cluster_name"
+    echo eval "\$(ibmcloud ks cluster config --export $cluster_name)"
+    exit 1
+  fi
+}
+
+docker_build() {
+  echo "Building docker image for $1"
+  pushd "$EIRINI_STAGING_BASEDIR"
+    docker build . -f "$EIRINI_STAGING_BASEDIR/image/$component/Dockerfile" \
+      --build-arg GIT_SHA=big-sha \
+      --tag "eirini/recipe-$component:patch-me-if-you-can"
+  popd
+}
+
+docker_push() {
+  echo "Pushing docker image for $1"
+  pushd "$EIRINI_STAGING_BASEDIR"
+    docker push "eirini/recipe-$component:patch-me-if-you-can"
+  popd
+}
+
+update_values_file() {
+  echo "Applying docker image of $1 to kubernetes cluster"
+  new_image_ref="$(docker images --format='{{.Digest}}' "eirini/recipe-$1" | grep sha)"
+  goml set --file $VALUES_FILE --prop "eirini.opi.staging.${1}_image_digest" --value "$new_image_ref"
+}
+
+helm_upgrade() {
+  pushd "$EIRINI_RELEASE_BASEDIR/helm/cf"
+    helm dep update
+  popd
+
+  local secret ca_cert secret_name bits_tls_crt bits_tls_key
+  secret=$(kubectl get pods --namespace uaa -o jsonpath='{.items[?(.metadata.name=="uaa-0")].spec.containers[?(.name=="uaa")].env[?(.name=="INTERNAL_CA_CERT")].valueFrom.secretKeyRef.name}')
+  ca_cert=$(kubectl get secret "$secret" --namespace uaa -o jsonpath="{.data['internal-ca-cert']}" | base64 --decode -)
+
+  secret_name=$(kubectl get secrets -n default | grep "$(current_cluster_name)" | cut -d ' ' -f 1)
+  bits_tls_crt=$(kubectl get secret "$secret_name" --namespace default -o jsonpath="{.data['tls\.crt']}" | base64 --decode -)
+  bits_tls_key=$(kubectl get secret "$secret_name" --namespace default -o jsonpath="{.data['tls\.key']}" | base64 --decode -)
+
+  echo "HELM version: $(helm version)"
+  pushd "$EIRINI_RELEASE_BASEDIR/helm"
+    helm upgrade --install scf ./cf \
+      --namespace scf \
+      --values "${VALUES_FILE}" \
+      --set "secrets.UAA_CA_CERT=${ca_cert}" \
+      --set "bits.secrets.BITS_TLS_KEY=${bits_tls_key}" \
+      --set "bits.secrets.BITS_TLS_CRT=${bits_tls_crt}"
+  popd
+}
+
+current_cluster_name() {
+  kubectl config current-context | cut -d / -f 1
+}
+
+restart_eirini(){
+	local pod_name=$(kubectl get pods -n scf --selector=name=eirini --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
+	kubectl delete pod $pod_name -n scf
+}
+
+main "$@"
